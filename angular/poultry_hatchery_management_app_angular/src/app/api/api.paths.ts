aeb208c7621249dd2c07934c451154c82814c97b
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-10-14 16:13:01 +0200

    Modified api paths structure (enums wrapped with namespace)

--- angular/poultry_hatchery_management_app_angular/src/app/api/api.paths.ts
+++ angular/poultry_hatchery_management_app_angular/src/app/api/api.paths.ts
@@ -1,10 +1,12 @@
 
-export enum AuthenticatePaths {
-    POST_VALIDATE = '/api/v1/auth/validate',
-    POST_REGISTER = '/api/v1/auth/register',
-    POST_REFRESH = '/api/v1/auth/refresh'
-}
+export namespace ApiPaths {
+    export enum AuthenticatePaths {
+        POST_VALIDATE = '/api/v1/auth/validate',
+        POST_REGISTER = '/api/v1/auth/register',
+        POST_REFRESH = '/api/v1/auth/refresh'
+    }
 
-export enum UserDataPaths {
-    GET_USER_SELF = '/api/v1/data/user/self',
+    export enum UserDataPaths {
+        GET_USER_SELF = '/api/v1/data/user/self',
+    }
 }
