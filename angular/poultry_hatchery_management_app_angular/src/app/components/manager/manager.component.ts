4866040626d7f1dd4280583ea4657fccfb54167b
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-10-22 03:21:11 +0200

    Modified ManagerComponent (added panel with all users)

--- angular/poultry_hatchery_management_app_angular/src/app/components/manager/manager.component.ts
+++ angular/poultry_hatchery_management_app_angular/src/app/components/manager/manager.component.ts
@@ -5,7 +5,11 @@ import { UserDetailsService } from '../../services/users/user-details.service';
 import { UserDetails } from '../../models/user-details.model';
 import { Observable } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-manager',
@@ -14,45 +18,63 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatCard,
     MatIcon,
     MatButtonModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    FormsModule
   ],
   templateUrl: './manager.component.html',
   styleUrl: './manager.component.css'
 })
 export class ManagerComponent {
-  public userFirstName: string = 'Imie';
-  public userLastName: string = 'Nazwisko';
-  public userRole: string = 'Rola';
-  public userEmail: string = 'Email';
-  public userOrganisationName: string = "Organizacja";
-  public userOrganisationRegon: string = "";
-  public userOrganisationAddress: string = "";
+  public adminFirstName: string = 'Imie';
+  public adminLastName: string = 'Nazwisko';
+  public adminRole: string = 'Rola';
+  public adminEmail: string = 'Email';
+  public adminOrganisationName: string = "Organizacja";
+  public adminOrganisationRegon: string = "";
+  public adminOrganisationAddress: string = "";
 
-  private userDetails$: Observable<UserDetails> | null = null;
+  public userDetailsAll: UserDetails[] | null = null;
+  public filteredUserDetailsAll: UserDetails[] | null = null;
+  public userCategory: string = '';
 
   public constructor (
     private userDetailsService: UserDetailsService
   ) {}
 
   ngOnInit() {
-    this.userDetails$ = this.userDetailsService.getUserDetails(); 
-    this.userDetails$.subscribe(userDetails => {
-      if (userDetails) {
-        this.userFirstName = userDetails.firstName;
-        this.userLastName = userDetails.lastName;
-        this.userRole = userDetails.role;
-        this.userEmail = userDetails.emailAddress;
-        this.userOrganisationName = userDetails.organisation.name;
-        this.userOrganisationRegon = userDetails.organisation.regon;
-        this.userOrganisationAddress = `${userDetails.organisation.address.postalCode}, 
-                                        ${userDetails.organisation.address.city}<br>
-                                        ul. ${userDetails.organisation.address.street} 
-                                        ${userDetails.organisation.address.number}`;
-        
-        
+    let adminDetails$ = this.userDetailsService.getUserDetails(); 
+    adminDetails$.subscribe(adminDetails => {
+      if (adminDetails) {
+        this.adminFirstName = adminDetails.firstName;
+        this.adminLastName = adminDetails.lastName;
+        this.adminRole = adminDetails.role;
+        this.adminEmail = adminDetails.emailAddress;
+        this.adminOrganisationName = adminDetails.organisation.name;
+        this.adminOrganisationRegon = adminDetails.organisation.regon;
+        this.adminOrganisationAddress = `${adminDetails.organisation.address.postalCode}, 
+                                        ${adminDetails.organisation.address.city}<br>
+                                        ul. ${adminDetails.organisation.address.street} 
+                                        ${adminDetails.organisation.address.number}`;
+         
       }
     });
+    let userDetailsAll$ = this.userDetailsService.getUserDetailsAll();
+    userDetailsAll$.subscribe(userDetails => {
+      this.userDetailsAll = userDetails;
+    })
   }
+
+  filterItems() {
+
+  }
+
+  onCategoryChange(event: any) {
+
+  }
+
 
 
 }
