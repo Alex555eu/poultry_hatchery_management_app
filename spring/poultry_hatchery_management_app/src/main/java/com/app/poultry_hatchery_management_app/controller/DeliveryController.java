f90d90dc9124871003f0b0631e694b8bbd312b9b
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-11-01 08:18:55 +0100

    Added ProductType model (used by Delivery model)

--- spring/poultry_hatchery_management_app/src/main/java/com/app/poultry_hatchery_management_app/controller/DeliveryController.java
+++ spring/poultry_hatchery_management_app/src/main/java/com/app/poultry_hatchery_management_app/controller/DeliveryController.java
@@ -1,8 +1,10 @@
 package com.app.poultry_hatchery_management_app.controller;
 
 import com.app.poultry_hatchery_management_app.dto.PostDeliveryRequest;
+import com.app.poultry_hatchery_management_app.dto.PostSupplierRequest;
 import com.app.poultry_hatchery_management_app.dto.PutDeliveryRequest;
 import com.app.poultry_hatchery_management_app.model.Delivery;
+import com.app.poultry_hatchery_management_app.model.ProductType;
 import com.app.poultry_hatchery_management_app.model.Supplier;
 import com.app.poultry_hatchery_management_app.service.DeliveryService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -12,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -63,23 +66,41 @@ public class DeliveryController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/product-types-all")
+    public ResponseEntity<String> getAllProductTypes() throws JsonProcessingException {
+        List<String> types = deliveryService.getAllProductTypes();
+        if (types.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        String response = objectMapper.writeValueAsString(types);
+        return ResponseEntity.ok(response);
+    }
+
     @PostMapping
     public ResponseEntity<String> postDelivery(@RequestBody PostDeliveryRequest request) {
-        Optional<Delivery> delivery = deliveryService.postDelivery(request);
-        if (delivery.isPresent()) {
-            return ResponseEntity.ok().build();
+        try {
+            Optional<Delivery> delivery = deliveryService.postDelivery(request);
+            if (delivery.isPresent()) {
+                return ResponseEntity.ok().build();
+            }
+            return ResponseEntity.notFound().build();
+        } catch (ResponseStatusException ex) {
+            return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
         }
-        return ResponseEntity.notFound().build();
     }
 
 
     @PutMapping
     public ResponseEntity<String> putDelivery(@RequestBody PutDeliveryRequest request) {
-        Optional<Delivery> delivery = deliveryService.putDelivery(request);
-        if (delivery.isPresent()) {
-            return ResponseEntity.ok().build();
+        try {
+            Optional<Delivery> delivery = deliveryService.putDelivery(request);
+            if (delivery.isPresent()) {
+                return ResponseEntity.ok().build();
+            }
+            return ResponseEntity.notFound().build();
+        } catch (ResponseStatusException ex) {
+            return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
         }
-        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping
@@ -88,6 +109,35 @@ public class DeliveryController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/supplier")
+    public ResponseEntity<String> getAllSuppliers() throws JsonProcessingException {
+        List<Supplier> suppliers = deliveryService.getAllSuppliers();
+        if (suppliers == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        } else if (suppliers.isEmpty()) {
+            return ResponseEntity.notFound().build();
+
+        } else {
+            String response = objectMapper.writeValueAsString(suppliers);
+            return ResponseEntity.ok(response);
+        }
+    }
+
+    @PostMapping("/supplier")
+    public ResponseEntity<String> postSupplier(@RequestBody PostSupplierRequest request) throws JsonProcessingException {
+        Optional<Supplier> supplier;
+        try {
+            supplier = deliveryService.postSupplier(request);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        if (supplier.isPresent()) {
+            String body = objectMapper.writeValueAsString(supplier.get());
+            return ResponseEntity.ok(body);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 
 
 }
