8c2d62634c44a379f68a264f9061f4164b35864d
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-11-09 14:19:57 +0100

    Added incubator component (select menu before actual incubators)

--- angular/poultry_hatchery_management_app_angular/src/app/app.routes.ts
+++ angular/poultry_hatchery_management_app_angular/src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { RegisterComponent } from './components/register/register.component';
 import { ManagerComponent } from './components/manager/manager.component';
 import { DeliveriesComponent } from './components/deliveries/deliveries.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { IncubatorsComponent } from './components/incubators/incubators.component';
 
 export const routes: Routes = [
     { path: '', component: NavbarComponent,
         children: [
             { path: 'manager', component: ManagerComponent},
             { path: 'deliveries', component: DeliveriesComponent},
-            { path: 'tasks', component: TasksComponent}
+            { path: 'tasks', component: TasksComponent},
+            { path: 'incubators', component: IncubatorsComponent}
         ],
     },
     { path: 'login', component: LoginComponent},
