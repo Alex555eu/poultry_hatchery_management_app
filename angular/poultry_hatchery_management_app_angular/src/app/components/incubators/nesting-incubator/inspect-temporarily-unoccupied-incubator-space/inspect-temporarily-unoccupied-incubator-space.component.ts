e373df31fc9adac90eaebbdae9a8f7ddd5bea692
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-11-14 00:32:57 +0100

    Added empty components (preparations for upcoming work)

---
+++ angular/poultry_hatchery_management_app_angular/src/app/components/incubators/nesting-incubator/inspect-temporarily-unoccupied-incubator-space/inspect-temporarily-unoccupied-incubator-space.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inspect-temporarily-unoccupied-incubator-space',
+  standalone: true,
+  imports: [],
+  templateUrl: './inspect-temporarily-unoccupied-incubator-space.component.html',
+  styleUrl: './inspect-temporarily-unoccupied-incubator-space.component.css'
+})
+export class InspectTemporarilyUnoccupiedIncubatorSpaceComponent {
+
+}
---
+++ angular/poultry_hatchery_management_app_angular/src/app/components/incubators/nesting-incubator/inspect-unoccupied-incubator-space/inspect-unoccupied-incubator-space.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inspect-unoccupied-incubator-space',
+  standalone: true,
+  imports: [],
+  templateUrl: './inspect-unoccupied-incubator-space.component.html',
+  styleUrl: './inspect-unoccupied-incubator-space.component.css'
+})
+export class InspectUnoccupiedIncubatorSpaceComponent {
+
+}
