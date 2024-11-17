80c075687997f4ea19f3cc2d302f7c6c93822600
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-11-17 17:49:30 +0100

    Added date-format pipes

---
+++ angular/poultry_hatchery_management_app_angular/src/app/utils/date-format/custom-date-formatter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { CustomDateFormatterService } from './custom-date-formatter.service';
+
+@Pipe({
+  name: 'customDateFormatter',
+  standalone: true
+})
+export class CustomDateFormatterPipe implements PipeTransform {
+
+  constructor(private dateFormatterService: CustomDateFormatterService) {}
+
+  transform(value: Date, formatType: 'default' | 'full' = 'default'): string {
+    if (!value) return 'Brak daty';
+
+    if (formatType === 'full') {
+      return this.dateFormatterService.formatDate2(value);
+    } else {
+      return this.dateFormatterService.formatDate(value);
+    }
+  }
+}
