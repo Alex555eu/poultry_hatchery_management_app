742a1b3f83d3a589e592dbf2739f88a873667b39
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-11-19 13:21:16 +0100

    Modified date formatters

--- angular/poultry_hatchery_management_app_angular/src/app/custom-date-adapter.ts
+++ angular/poultry_hatchery_management_app_angular/src/app/custom-date-adapter.ts
@@ -4,6 +4,6 @@ import { NativeDateAdapter } from '@angular/material/core';
 @Injectable()
 export class CustomDateAdapter extends NativeDateAdapter { 
   override getFirstDayOfWeek(): number {
-    return 1; // set first day in calendars to monday(1), instead of sunday(0)
+    return 1; // sets first day in calendars to monday(1), instead of sunday(0)
   }
 }
