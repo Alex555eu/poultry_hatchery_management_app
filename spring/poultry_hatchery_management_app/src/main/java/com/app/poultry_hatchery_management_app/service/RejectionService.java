ad072aa484d518110d5bae244487e6be16704e3f
Alex555eu <aleksanderpasichofficial@gmail.com>
2024-11-23 13:36:25 +0100

    Modified RejectionService.java (file clean up)

--- spring/poultry_hatchery_management_app/src/main/java/com/app/poultry_hatchery_management_app/service/RejectionService.java
+++ spring/poultry_hatchery_management_app/src/main/java/com/app/poultry_hatchery_management_app/service/RejectionService.java
@@ -41,40 +41,8 @@ public class RejectionService {
         return RejectionCause.getAvailableCauses(RejectionGroup.REJECTION_1);
     }
 
-    public List<RejectionCause> getPossibleRejection2Causes() {
-        return RejectionCause.getAvailableCauses(RejectionGroup.REJECTION_2);
-    }
-
-    public List<RejectionCause> getPossibleRejection3Causes() {
-        return RejectionCause.getAvailableCauses(RejectionGroup.REJECTION_3);
-    }
-
-    public List<RejectionCause> getPossibleRejection4Causes() {
-        return RejectionCause.getAvailableCauses(RejectionGroup.REJECTION_4);
-    }
-
     public List<Rejection1> getAllRejections1ByNestingId(UUID nestingId) {
         return rejection1Repository.findAllByNestingId(nestingId);
-    }
-
-    public List<Rejection2> getAllRejections2ByCandlingId(UUID candlingId) {
-        return rejection2Repository.findAllByCandlingId(candlingId);
-    }
-
-    public List<Rejection2> getAllRejections2ByNestingId(UUID nestingId) {
-        return rejection2Repository.findAllByNestingId(nestingId);
-    }
-
-    public List<Rejection3> getAllRejections3ByNestingId(UUID nestingId) {
-        return rejection3Repository.findAllByNestingId(nestingId);
-    }
-
-    public List<Rejection4> getAllRejections4ByNestingId(UUID nestingId) {
-        return rejection4Repository.findAllByNestingId(nestingId);
-    }
-
-    public List<RejectionUnexpected> getAllUnexpectedRejectionsByNestingId(UUID nestingId) {
-        return rejectionUnexpectedRepository.findAllByNestingId(nestingId);
     }
 
     public Optional<Rejection1> postRejection1(PostRejection1Request request) {
@@ -91,6 +59,36 @@ public class RejectionService {
             return Optional.of(rejection1);
         }
         return Optional.empty();
+    }
+
+    public Optional<Rejection1> putRejection1(PutRejectionRequest request) {
+        Optional<Rejection1> rejection1 = rejection1Repository.findById(request.rejectionId());
+        if (rejection1.isPresent()) {
+            rejection1.get().setQuantity(request.quantity());
+            rejection1.get().setCause(RejectionCause.valueOf(request.cause()).verify(RejectionGroup.REJECTION_1));
+            rejection1Repository.save(rejection1.get());
+
+            return rejection1;
+        }
+        return Optional.empty();
+    }
+
+    public void deleteRejection1ById(UUID rejectionId) {
+        rejection1Repository.deleteById(rejectionId);
+    }
+
+
+
+    public List<Rejection2> getAllRejections2ByCandlingId(UUID candlingId) {
+        return rejection2Repository.findAllByCandlingId(candlingId);
+    }
+
+    public List<Rejection2> getAllRejections2ByNestingId(UUID nestingId) {
+        return rejection2Repository.findAllByNestingId(nestingId);
+    }
+
+    public List<RejectionCause> getPossibleRejection2Causes() {
+        return RejectionCause.getAvailableCauses(RejectionGroup.REJECTION_2);
     }
 
     @Transactional
@@ -147,6 +145,16 @@ public class RejectionService {
         }
     }
 
+
+
+    public List<Rejection3> getAllRejections3ByNestingId(UUID nestingId) {
+        return rejection3Repository.findAllByNestingId(nestingId);
+    }
+
+    public List<RejectionCause> getPossibleRejection3Causes() {
+        return RejectionCause.getAvailableCauses(RejectionGroup.REJECTION_3);
+    }
+
     public Optional<Rejection3> postRejection3(PostRejection3Request request) {
         Optional<HatchingLoadedDeliveries> hld =
                 hatchingService.getHatchingLoadedDeliveryById(request.hatchingLoadedDeliveryId());
@@ -163,6 +171,32 @@ public class RejectionService {
         return Optional.empty();
     }
 
+    public Optional<Rejection3> putRejection3(PutRejectionRequest request) {
+        Optional<Rejection3> rejection3 = rejection3Repository.findById(request.rejectionId());
+        if (rejection3.isPresent()) {
+            rejection3.get().setQuantity(request.quantity());
+            rejection3.get().setCause(RejectionCause.valueOf(request.cause()).verify(RejectionGroup.REJECTION_3));
+            rejection3Repository.save(rejection3.get());
+
+            return rejection3;
+        }
+        return Optional.empty();
+    }
+
+    public void deleteRejection3ById(UUID rejectionId) {
+        rejection3Repository.deleteById(rejectionId);
+    }
+
+
+
+    public List<Rejection4> getAllRejections4ByNestingId(UUID nestingId) {
+        return rejection4Repository.findAllByNestingId(nestingId);
+    }
+
+    public List<RejectionCause> getPossibleRejection4Causes() {
+        return RejectionCause.getAvailableCauses(RejectionGroup.REJECTION_4);
+    }
+
     public Optional<Rejection4> postRejection4(PostRejection4Request request) {
         Optional<HatchingResult> result =
                 hatchingService.getHatchingResultsById(request.hatchingResultId());
@@ -177,6 +211,29 @@ public class RejectionService {
             return Optional.of(rejection4);
         }
         return Optional.empty();
+    }
+
+    public Optional<Rejection4> putRejection4(PutRejectionRequest request) {
+        Optional<Rejection4> rejection4 = rejection4Repository.findById(request.rejectionId());
+        if (rejection4.isPresent()) {
+            rejection4.get().setQuantity(request.quantity());
+            rejection4.get().setCause(RejectionCause.valueOf(request.cause()).verify(RejectionGroup.REJECTION_4));
+            rejection4Repository.save(rejection4.get());
+
+            return rejection4;
+        }
+        return Optional.empty();
+    }
+
+
+    public void deleteRejection4ById(UUID rejectionId) {
+        rejection4Repository.deleteById(rejectionId);
+    }
+
+
+
+    public List<RejectionUnexpected> getAllUnexpectedRejectionsByNestingId(UUID nestingId) {
+        return rejectionUnexpectedRepository.findAllByNestingId(nestingId);
     }
 
     public Optional<RejectionUnexpected> postUnexpectedRejection(PostUnexpectedRejectionRequest request) {
@@ -196,43 +253,6 @@ public class RejectionService {
         return Optional.empty();
     }
 
-    public Optional<Rejection1> putRejection1(PutRejectionRequest request) {
-        Optional<Rejection1> rejection1 = rejection1Repository.findById(request.rejectionId());
-        if (rejection1.isPresent()) {
-            rejection1.get().setQuantity(request.quantity());
-            rejection1.get().setCause(RejectionCause.valueOf(request.cause()).verify(RejectionGroup.REJECTION_1));
-            rejection1Repository.save(rejection1.get());
-
-            return rejection1;
-        }
-        return Optional.empty();
-    }
-
-
-    public Optional<Rejection3> putRejection3(PutRejectionRequest request) {
-        Optional<Rejection3> rejection3 = rejection3Repository.findById(request.rejectionId());
-        if (rejection3.isPresent()) {
-            rejection3.get().setQuantity(request.quantity());
-            rejection3.get().setCause(RejectionCause.valueOf(request.cause()).verify(RejectionGroup.REJECTION_3));
-            rejection3Repository.save(rejection3.get());
-
-            return rejection3;
-        }
-        return Optional.empty();
-    }
-
-    public Optional<Rejection4> putRejection4(PutRejectionRequest request) {
-        Optional<Rejection4> rejection4 = rejection4Repository.findById(request.rejectionId());
-        if (rejection4.isPresent()) {
-            rejection4.get().setQuantity(request.quantity());
-            rejection4.get().setCause(RejectionCause.valueOf(request.cause()).verify(RejectionGroup.REJECTION_4));
-            rejection4Repository.save(rejection4.get());
-
-            return rejection4;
-        }
-        return Optional.empty();
-    }
-
     public Optional<RejectionUnexpected> putUnexpectedRejection(PutRejectionRequest request) {
         Optional<RejectionUnexpected> rejectionUnexpected = rejectionUnexpectedRepository.findById(request.rejectionId());
         if (rejectionUnexpected.isPresent()) {
@@ -243,19 +263,6 @@ public class RejectionService {
             return rejectionUnexpected;
         }
         return Optional.empty();
-    }
-
-    public void deleteRejection1ById(UUID rejectionId) {
-        rejection1Repository.deleteById(rejectionId);
-    }
-
-
-    public void deleteRejection3ById(UUID rejectionId) {
-        rejection3Repository.deleteById(rejectionId);
-    }
-
-    public void deleteRejection4ById(UUID rejectionId) {
-        rejection4Repository.deleteById(rejectionId);
     }
 
     public void deleteUnexpectedRejectionById(UUID rejectionId) {
